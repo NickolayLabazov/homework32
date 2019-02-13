eaf6ae46ba3e0a802d9bcce43d22b9fb8cb3e8b4
Николай Лабазов <Nickolaylabazov@yandex.ru>
2019-02-13 20:30:43 +0300

    correction 3

--- test/test.js
+++ test/test.js
@@ -1,33 +1,33 @@
 import sortPerson from '../function';
 
-test (`Сортировка по здоровью`, () => {
-	const input = [
-  {name: 'Мечник', health: 10},
-  {name: 'Маг', health: 100},
-  {name: 'Лучник', health: 80}
-]
-	const expected = [
-  {name: 'Маг', health: 100},
-  {name: 'Лучник', health: 80},
-  {name: 'Мечник', health: 10}
-]
-	const received = sortPerson (input);
-	
-	expect(received).toEqual(expected);
-})
+test('Сортировка по здоровью', () => {
+  const input = [
+    { name: 'Мечник', health: 10 },
+    { name: 'Маг', health: 100 },
+    { name: 'Лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'Маг', health: 100 },
+    { name: 'Лучник', health: 80 },
+    { name: 'Мечник', health: 10 },
+  ];
+  const received = sortPerson(input);
 
-test (`Сортировка по здоровью`, () => {
-	const input = [
-  {name: 'Мечник', health: 100},
-  {name: 'Маг', health: 10},
-  {name: 'Лучник', health: 80}
-]
-	const expected = [
-  {name: 'Мечник', health: 100},
-  {name: 'Лучник', health: 80},
-  {name: 'Маг', health: 10}
-]
-	const received = sortPerson (input);
-	
-	expect(received).toEqual(expected);
-})
+  expect(received).toEqual(expected);
+});
+
+test('Сортировка по здоровью', () => {
+  const input = [
+    { name: 'Мечник', health: 100 },
+    { name: 'Маг', health: 10 },
+    { name: 'Лучник', health: 80 },
+  ];
+  const expected = [
+    { name: 'Мечник', health: 100 },
+    { name: 'Лучник', health: 80 },
+    { name: 'Маг', health: 10 },
+  ];
+  const received = sortPerson(input);
+
+  expect(received).toEqual(expected);
+});
